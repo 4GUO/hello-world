ce6189aa4f5c5f1f5e080b1540a11d785bf8db96
4man <foreach@aliyun.com>
2021-02-02 11:07:58 +0800

    base

---
+++ example/test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * User: 4man
+ * Date: 2021/2/2
+ * Time: 10:40 上午
+ */
+
+require_once "../src/HelloWorld.php";
+
+use Bin918\HelloWorld;
+
+HelloWorld::run();
---
+++ src/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: 4man
+ * Date: 2021/2/1
+ * Time: 10:04 上午
+ */
+
+namespace Bin918;
+
+class HelloWorld {
+
+    public static function run() {
+        print_r('hello world ( a test package )');
+    }
+
+}
